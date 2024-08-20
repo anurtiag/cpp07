f5dde335c78da707efdfe9286f9cdbf391123023
Antton Urtiaga <anurtiag@c3r1s5.42urduliz.com>
2024-08-20 17:07:01 +0200

    garbanzo

--- ex02/Array.cpp
+++ ex02/Array.cpp
@@ -6,10 +6,31 @@
 /*   By: anurtiag <anurtiag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 12:25:40 by anurtiag          #+#    #+#             */
-/*   Updated: 2024/08/20 14:56:06 by anurtiag         ###   ########.fr       */
+/*   Updated: 2024/08/20 15:23:26 by anurtiag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-template <typedef T> Array::
+template <typename T> Array<T>::Array()
+{
+    this->array = new[0];
+    this->length = 0;
+}
+
+template <typename T> Array<T>::Array(const unsigned int& length)
+{
+    this->array = new[length];
+    this->length = length;
+}
+
+template <typename T> Array<T>::Array(const Array<T>& source)
+{
+    *this = source;
+}
+
+template <typename T> Array<T>& Array<T>::operator=(const Array<T>& source)
+{
+    delete[] this->array;
+    this.
+}
--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: anurtiag <anurtiag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 12:25:37 by anurtiag          #+#    #+#             */
-/*   Updated: 2024/08/20 14:51:54 by anurtiag         ###   ########.fr       */
+/*   Updated: 2024/08/20 15:22:14 by anurtiag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ template <typename T> class Array
         Array();
         Array(const unsigned int& length);
         Array(const Array<T>& source);
-        Array& operator=(const Array<T>& source);
+        Array<T>& operator=(const Array<T>& source);
         T& operator[](int i);
 };
 
